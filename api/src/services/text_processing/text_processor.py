37ed65d3650af69f3ae67819a04df9818f5e45dc
Fireblade <fireblade5234@gmail.com>
2025-02-28 21:33:00 -0500

    better method of preserving custom phonemes through the normalizer

--- api/src/services/text_processing/text_processor.py
+++ api/src/services/text_processing/text_processor.py
@@ -2,7 +2,7 @@
 
 import re
 import time
-from typing import AsyncGenerator, List, Tuple
+from typing import AsyncGenerator, Dict, List, Tuple
 
 from loguru import logger
 
@@ -11,6 +11,9 @@ from .normalizer import normalize_text
 from .phonemizer import phonemize
 from .vocabulary import tokenize
 from ...structures.schemas import NormalizationOptions
+
+# Pre-compiled regex patterns for performance
+CUSTOM_PHONEMES = re.compile(r"(\[([^\]]|\n)*?\])(\(\/([^\/)]|\n)*?\/\))")
 
 def process_text_chunk(
     text: str, language: str = "a", skip_phonemize: bool = False
@@ -84,53 +87,22 @@ def process_text(text: str, language: str = "a") -> List[int]:
 
     return process_text_chunk(text, language)
 
-def get_protected_areas(text: str) -> List[Tuple[int,int]]:
-    protected_areas = []
-    start_index = -1
-    level = 0
-    for char in range(0, len(text)):
-        if text[char] in "([{":
-            if level == 0:
-                start_index = char
-            level+=1
-        elif text[char] in ")]}":
-            level=max(level - 1,0)
-            if level == 0 and start_index != -1:
-                protected_areas.append((start_index,char))
-                start_index = -1
-    if level > 0:
-        protected_areas.append((start_index,len(text) - 1))
-        
-    return protected_areas
 
-def get_sentence_info(text: str, protected_areas: List[Tuple[int,int]]) -> List[Tuple[str, List[int], int]]:
+def get_sentence_info(text: str, custom_phenomes_list: Dict[str, str]) -> List[Tuple[str, List[int], int]]:
     """Process all sentences and return info."""
+    sentences = re.split(r"([.!?;:])(?=\s|$)", text)
+    phoneme_length, min_value = len(custom_phenomes_list), 0
     
-    sentences = [text]
-    text+=" "
-    
-    last_split_point = 0
-    protection_index = 0
-    protected_areas_count = len(protected_areas)
-    for char in range(0, len(text) - 1):
-        if text[char] in ".!?;:]" and text[char + 1] == " ":
-            if protected_areas_count == 0 or (char < protected_areas[protection_index][0] and char > protected_areas[protection_index][1]):
-                last_item = sentences[-1]
-                
-                sentences[-1] = last_item[:char - last_split_point + 1].strip()
-                split_item = last_item[char - last_split_point + 1:].strip()
-                if split_item != "":
-                    sentences.append(split_item)
-                last_split_point = char + 1
-                
-        if protected_areas_count > 0:
-            if char >= protected_areas[protection_index][1]:
-                protection_index = min(protection_index + 1, protected_areas_count)
-        
-    print(sentences)
     results = []
     for i in range(0, len(sentences), 2):
         sentence = sentences[i].strip()
+        for replaced in range(min_value, phoneme_length):
+            current_id = f"</|custom_phonemes_{replaced}|/>"
+            if current_id in sentence:
+                sentence = sentence.replace(current_id, custom_phenomes_list.pop(current_id))
+                min_value += 1
+                
+            
         punct = sentences[i + 1] if i + 1 < len(sentences) else ""
 
         if not sentence:
@@ -142,6 +114,10 @@ def get_sentence_info(text: str, protected_areas: List[Tuple[int,int]]) -> List[
 
     return results
 
+def handle_custom_phonemes(s: re.Match[str], phenomes_list: Dict[str,str]) -> str:
+    latest_id = f"</|custom_phonemes_{len(phenomes_list)}|/>"
+    phenomes_list[latest_id] = s.group(0).strip()
+    return latest_id
 
 async def smart_split(
     text: str, 
@@ -154,16 +130,18 @@ async def smart_split(
     chunk_count = 0
     logger.info(f"Starting smart split for {len(text)} chars")
 
+    custom_phoneme_list = {}
+
     # Normalize text
     if settings.advanced_text_normalization and normalization_options.normalize:
         if lang_code in ["a","b","en-us","en-gb"]:
+            text = CUSTOM_PHONEMES.sub(lambda s: handle_custom_phonemes(s, custom_phoneme_list), text)
             text=normalize_text(text,normalization_options)
         else:
             logger.info("Skipping text normalization as it is only supported for english")
 
     # Process all sentences
-    protected_areas = get_protected_areas(text)
-    sentences = get_sentence_info(text, protected_areas)
+    sentences = get_sentence_info(text, custom_phoneme_list)
 
     current_chunk = []
     current_tokens = []
